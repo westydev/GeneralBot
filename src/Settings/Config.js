1d43d6d54f9585f2442c30e1c0de52df110cb4a8
Miraç <88139688+westydev@users.noreply.github.com>
2022-12-05 23:11:45 +0400

    Update Config.js

--- src/Settings/Config.js
+++ src/Settings/Config.js
@@ -1,8 +1,8 @@
 module.exports = {
     BOT: {
-        token: "MTA0OTM2MjIwODU4MjI3MTAzNw.GHS3_4.LH34MxXw3DVLv2t3ar7a_EqJhs4_dq-HzfBVkM",
-        appID: "1049362208582271037", 
-        guildId: "1049361460251340810",
+        token: "",
+        appID: "", 
+        guildId: "",
         HandlerMode: "Guild",
         ownerId: ""
     }
