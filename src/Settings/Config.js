c761fdb16dc1c4a9a1364fb83dc3460b561842d3
Miraç <88139688+westydev@users.noreply.github.com>
2022-12-10 22:16:19 +0400

    Update Config.js

--- src/Settings/Config.js
+++ src/Settings/Config.js
@@ -1,13 +1,13 @@
 module.exports = {
     BOT: {
-        token: "MTA0OTM2MjIwODU4MjI3MTAzNw.GnO2mC.iegpGgvmsuEDkpJzklmC2i9VNKvuwWeYKIUFVQ",
-        appID: "1049362208582271037", 
+        token: "",
+        appID: "", 
         botActivity: "Discord.JS V14 GeneralBot",
-        guildId: "1049361460251340810",
+        guildId: "",
         HandlerMode: "Guild",
-        owners: ["993754957985632266"]
+        owners: [""]
     },
     DATABASE: {
-        mongooseConnection: "mongodb+srv://westydev:westydev1337@westydevbots.j8ngkko.mongodb.net/GeneralBot?retryWrites=true&w=majority"
+        mongooseConnection: ""
     }
 }
